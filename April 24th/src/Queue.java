92ced3cf5f02a16ace281498dd8c29b4ddadacd1
SortelOne <Sortel1@yahoo.com>
2018-04-26 14:27:30 -0600

    GenericQueue

--- April 24th/src/Queue.java
+++ April 24th/src/Queue.java
@@ -1,7 +1,57 @@
 import java.util.ArrayList;
 
-public class Queue {
-    String[] myArray = new String[5];
+public class Queue<E> {
+    private java.util.ArrayList<E> myList;
 
-    ArrayList<String> myList = new ArrayList<String>();
+    public Queue(){
+        myList = new java.util.ArrayList<E>();
+    }
+
+    public int getSize(){
+        return myList.size();
+    }
+
+    //make different somehow \|/
+    public E peek(){
+        return myList.get(0);
+    }
+
+    public E dequeue(){
+        return myList.remove(0);
+    }
+
+    public void enqueue(E o){
+        myList.add(o);
+    }
+
+    public boolean isEmpty(){
+        return (myList.size()==0);
+    }
+
+    public String toString(){
+        String ryan = "";
+        for(E sean: myList){
+            ryan += sean.toString()+"\n";
+        }
+        return ryan;
+    }
+    
+    public static void main(String[]args){
+        Queue<String> stackThingy = new Queue<String>();
+        stackThingy.enqueue("Nintendo Switch Games:");
+        stackThingy.enqueue("Super Mario Odyssey");
+        stackThingy.enqueue("LOZ: Breath of the Wild");
+        stackThingy.enqueue("Mario Kart 8 Deluxe");
+        if(!stackThingy.isEmpty())
+            stackThingy.dequeue();
+        System.out.println(stackThingy);
+        stackThingy.enqueue("Octopath: Traveler");
+        stackThingy.enqueue("Skyrim");
+        if(!stackThingy.isEmpty())
+            stackThingy.dequeue();
+        System.out.println(stackThingy);
+        System.out.println(stackThingy.peek());
+        System.out.println(stackThingy.isEmpty());
+        System.out.println(stackThingy.getSize());
+    }
 }
--- WEED/src/GenericStack.java
+++ WEED/src/GenericStack.java
@@ -4,35 +4,47 @@ public class GenericStack<E> {
     public GenericStack(){
         stack = new java.util.ArrayList<E>();
     }
-    public E getSize(int index){
-        return stack.get(index);
-    }
-    public int peek(){
+    public int getSize(){
         return stack.size();
     }
-    public E pop(){
+    public E peek(){
         return stack.get(stack.size()-1);
+    }
+    public E pop(){
+        return stack.remove(stack.size()-1);
     }
     public void push(E o){
         stack.add(o);
     }
     public boolean isEmpty(){
-        return !(stack.size()==0);
+        return (stack.size()==0);
+    }
+
+    public String toString(){
+        String kelson = "";
+        for(E garrett: stack){
+            kelson += garrett.toString()+"\n";
+        }
+        return kelson;
     }
 
     public static void main(String[]args){
         GenericStack<String> stackThingy = new GenericStack<String>();
-        System.out.println(stackThingy.isEmpty());
         stackThingy.push("Klesoron");
         stackThingy.push("Grandma Blats");
         stackThingy.push("Garrett");
         stackThingy.push("Sean");
+        if(!stackThingy.isEmpty())
+            stackThingy.pop();
+        System.out.println(stackThingy);
         stackThingy.push("Master Windu");
         stackThingy.push("Ryan");
         stackThingy.push("Arbitraty human name");
-        stackThingy.pop();
+        if(!stackThingy.isEmpty())
+            stackThingy.pop();
+        System.out.println(stackThingy);
         System.out.println(stackThingy.peek());
         System.out.println(stackThingy.isEmpty());
-        System.out.println(stackThingy.getSize(1));
+        System.out.println(stackThingy.getSize());
     }
 }
